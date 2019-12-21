406fcff8445287d8edf66c5844f02af0c337eaaa
Micah Silverman <micah@afitnerd.com>
2019-12-21 21:31:18 +0200

    added tree view with yellow indicator for id token and red indicator for access token

--- popup/implicit_offenses.js
+++ popup/implicit_offenses.js
@@ -1,7 +1,6 @@
 function listenForClicks() {
-  document.addEventListener("click", (e) => {
-
-    if (e.target.classList.contains("reset")) {
+  document.addEventListener('click', (e) => {
+    if (e.target.classList.contains('reset')) {
       browser.runtime.getBackgroundPage().then((backgroundPage) => {
         removeExistingOffenses();
         backgroundPage.reset();
@@ -11,22 +10,71 @@ function listenForClicks() {
 }
 
 function removeExistingOffenses() {
-  if (document.querySelector(".offenses")) {
-    document.querySelector(".offenses").remove();
+  var offenses = document.querySelector('.offenses');
+  if (offenses) {
+    offenses.remove();
   }
+}
+
+function json_tree(data, isNested) {
+  var json = (isNested) ? '<ul class="nested">' : '<ul class="offenses" id="myUL">';
+
+  for(var i = 0; i < data.length; ++i) {
+    if(data[i].data.length) {
+      json += '<li><span class="' + ((data[i].caret)?data[i].caret:'caret') + '">' + data[i].text + '</span>';
+      json += json_tree(data[i].data, true);
+    } else {
+      json += '<li>' + data[i].text;
+    }
+    json += '</li>';
+  }
+  return json + '</ul>';
+}
+
+function addTokenData(name, caret, tokenData, datum) {
+  if (tokenData.claims) {
+    var idx = datum.data.length;
+    datum.data.push({text: name, data: [{text: 'token value', data: []}], caret: caret})
+    datum.data[idx].data[0].data.push({text: tokenData.token, data: []})
+    datum.data[idx].data.push({text: 'token claims', data: []})
+    datum.data[idx].data[1].data.push({text: '<pre>' + JSON.stringify(tokenData.claims,2) + '</pre>', data: []});
+  } else {
+    datum.data.push({text: name, data: [{text: tokenData.token, data: []}, caret]})
+  }
+}
+
+function transformOffenses(offenses) {
+  var data = [];
+  const keys = Object.keys(offenses);
+  for (const key of keys) {
+    var datum = {text: key, data: []}
+    if (offenses[key].idToken) {
+      addTokenData('ID Token', 'caret-yellow', offenses[key].idToken, datum);
+    }
+    if (offenses[key].accessToken) {
+      addTokenData('Access Token', 'caret-red', offenses[key].accessToken, datum);
+    }
+    data.push(datum);
+  }
+  return data;
+}
+
+function setupToggler() {
+  ['caret', 'caret-yellow', 'caret-red'].forEach((className) => {
+    Array.prototype.forEach.call(document.getElementsByClassName(className), function (toggler) {
+      toggler.addEventListener('click', function() {
+        this.parentElement.querySelector('.nested').classList.toggle('active');
+        this.classList.toggle('caret-down');
+      });
+    })
+  });
 }
 
 function refreshExistingOffenses(backgroundPage) {
   removeExistingOffenses();
-  let ul = document.createElement("ul");
-  ul.className = "offenses";
-  const keys = Object.keys(backgroundPage.offenses);
-  for (const key of keys) {
-    let li = document.createElement("li");
-    li.innerHTML = key;
-    ul.appendChild(li);
-  }
-  document.querySelector('#sites').appendChild(ul);
+  var data = transformOffenses(backgroundPage.offenses);
+  document.querySelector('#sites').innerHTML = json_tree(data, false);
+  setupToggler();
 }
 
 browser.runtime.getBackgroundPage().then(refreshExistingOffenses);
